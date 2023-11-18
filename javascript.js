4eb85bffa857e1d28758f5fd2186fe0b1c02fcf3
Charles Kwang <11234292+KojinKuro@users.noreply.github.com>
2023-11-17 19:24:51 -0800

    Add basic files and logic
    The basic index.html, style.css, and script was added. The script has
    the functions of add, subtract, multiply, and divide added to it. It
    will return the value and output to the console what it is doing.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function add(a,b) {
+    console.log(`${a} + ${b} = ${a+b}`);
+    return a + b;
+}
+
+function subtract(a,b) {
+    console.log(`${a} - ${b} = ${a-b}`);
+    return a - b;
+}
+
+function multiply(a,b) {
+    console.log(`${a} x ${b} = ${a*b}`);
+    return a * b;
+}
+
+function divide(a,b) {
+    console.log(`${a} / ${b} = ${a/b}`);
+    return a/b;
+}
