eaaa4ceb851cbd1d75e237e3e6eb893a9b1e7f51
Charles Kwang <11234292+KojinKuro@users.noreply.github.com>
2023-11-18 22:44:11 -0800

    Add clear, delete, and inputs to display
    All the buttons are sent to the display. This is so the user can see
    what is going on in the backend. They are also being stored in the code
    through a local variable so it can be used later. A clear button was
    added to reset the calculator. A delete button was added to remove the
    last input in the input display.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
+const calcInputNode = document.querySelector('.calculator-input');
+let inputString = '';
+
+//buttons who's inputs get displayed
+document.querySelectorAll('button.display').forEach((button) => {
+    button.addEventListener('click', () => {
+        calcInputNode.innerText += button.dataset.display;
+        inputString += button.dataset.display;
+    });
+});
+//clear button
+document.querySelector('button#clear').addEventListener('click', () => {
+    calcInputNode.innerText = '';
+    inputString = '';
+});
+//delete button
+document.querySelector('button#delete').addEventListener('click', () => {
+    calcInputNode.innerText = calcInputNode.innerText.slice(0,-1);
+    inputString = inputString.slice(0,-1);
+});
+
+
 function add(a,b) {
     console.log(`${a} + ${b} = ${a+b}`);
     return a + b;
@@ -18,10 +40,6 @@ function divide(a,b) {
     return a/b;
 }
 
-function Calculator() {
-    
-}
-
-function operate(op,a,b) {
+function operate(a,op,b) {
     return op(a,b);
 }
