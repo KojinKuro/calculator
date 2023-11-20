3e1ecd6feb872ddabfd0c3ea60b99122bd9c6769
Charles Kwang <11234292+KojinKuro@users.noreply.github.com>
2023-11-20 03:02:11 -0800

    Process inputs, helper functions, handle decimals
    The buttons will automatically handle inputs so that way we don't get
    anything messed up like multiple functions back to back. The inputString
    will be formatted into an array with numbers and functions so that
    way when it runs it will automatically when run through the operate
    function. These are all hooked to run and get displayed when the equal
    button is hit. Decimals are now only able to appear once. Several helper
    functions were added to the code: checking if a value is a operation,
    is numeric, converting a string to a number, and converting a string to
    an operation function.

--- javascript.js
+++ javascript.js
@@ -1,45 +1,146 @@
-const calcInputNode = document.querySelector('.calculator-input');
-let inputString = '';
+const calcInputNode = document.querySelector(".calculator-input");
+const calcResultNode = document.querySelector(".calculator-result");
+let inputString = "";
+let decimalDisabled = false;
 
 //buttons who's inputs get displayed
-document.querySelectorAll('button.display').forEach((button) => {
-    button.addEventListener('click', () => {
-        calcInputNode.innerText += button.dataset.display;
-        inputString += button.dataset.display;
-    });
+document.querySelectorAll("button.display").forEach((button) => {
+  button.addEventListener("click", () => {
+    inputString += button.dataset.display;
+    calcInputNode.innerText = inputString;
+  });
+});
+//same as above but for functions which have special rules
+document.querySelectorAll("button.function").forEach((button) => {
+  button.addEventListener("click", () => {
+    if (inputString == "") return;
+    decimalDisabled = false;
+
+    let lastInput = inputString.slice(-1);
+    if (isOperation(lastInput)) inputString = inputString.slice(0, -1);
+    inputString += button.dataset.display;
+    calcInputNode.innerText = inputString;
+  });
+});
+//handles decimal button
+document.querySelector("button.decimal").addEventListener("click", () => {
+  if (decimalDisabled) return;
+
+  let lastInput = inputString.slice(-1);
+  if (isOperation(lastInput) || lastInput == "" || lastInput == "=")
+    inputString += "0";
+  inputString += document.querySelector("button.decimal").dataset.display;
+  calcInputNode.innerText = inputString;
+  decimalDisabled = true;
+});
+//equal code
+document.querySelector("button.equal").addEventListener("click", () => {
+  calcResultNode.innerText = operateArray(formatInput(inputString));
 });
 //clear button
-document.querySelector('button#clear').addEventListener('click', () => {
-    calcInputNode.innerText = '';
-    inputString = '';
+document.querySelector("button#clear").addEventListener("click", () => {
+  inputString = "";
+  decimalDisabled = false;
+  calcInputNode.innerText = inputString;
 });
 //delete button
-document.querySelector('button#delete').addEventListener('click', () => {
-    calcInputNode.innerText = calcInputNode.innerText.slice(0,-1);
-    inputString = inputString.slice(0,-1);
+document.querySelector("button#delete").addEventListener("click", () => {
+  let lastInput = inputString.slice(-1);
+  if (lastInput == ".") decimalDisabled = false;
+  inputString = inputString.slice(0, -1);
+  calcInputNode.innerText = inputString;
 });
 
-
-function add(a,b) {
-    console.log(`${a} + ${b} = ${a+b}`);
-    return a + b;
+function add(a, b) {
+  console.log(`${a} + ${b} = ${a + b}`);
+  return a + b;
 }
 
-function subtract(a,b) {
-    console.log(`${a} - ${b} = ${a-b}`);
-    return a - b;
+function subtract(a, b) {
+  console.log(`${a} - ${b} = ${a - b}`);
+  return a - b;
 }
 
-function multiply(a,b) {
-    console.log(`${a} x ${b} = ${a*b}`);
-    return a * b;
+function multiply(a, b) {
+  console.log(`${a} x ${b} = ${a * b}`);
+  return a * b;
 }
 
-function divide(a,b) {
-    console.log(`${a} / ${b} = ${a/b}`);
-    return a/b;
+function divide(a, b) {
+  console.log(`${a} / ${b} = ${a / b}`);
+  return a / b;
 }
 
-function operate(a,op,b) {
-    return op(a,b);
+function operate(a, op, b) {
+  return op(a, b);
+}
+
+function isOperation(text) {
+  return text === "+" || text === "-" || text === "x" || text === "/";
+}
+
+// function isInt(n) {
+//   return n % 1 === 0;
+// }
+
+function isNumeric(str) {
+  if (typeof str != "string") return false;
+  return !isNaN(str) && !isNaN(parseFloat(str));
+}
+
+function convertToNum(str) {
+  if (typeof str != "string") return;
+  return str * 1;
+}
+
+function convertToOp(str) {
+  switch (str) {
+    case "+":
+      return add;
+    case "-":
+      return subtract;
+    case "x":
+      return multiply;
+    case "/":
+      return divide;
+  }
+}
+
+function formatInput(str) {
+  let strArray = str.split("");
+  let data = [];
+  let curInput = "";
+
+  while (strArray.length) {
+    let strFirst = strArray.shift();
+    if (isNumeric(`${curInput}${strFirst}`)) {
+      curInput += strFirst;
+      if (!strArray.length) data.push(convertToNum(curInput));
+    } else {
+      data.push(convertToNum(curInput));
+      curInput = "";
+      data.push(convertToOp(strFirst));
+    }
+  }
+
+  if (data.length % 2 == 0) {
+    if (data[data.length - 1] == add || data[data.length - 1] == subtract)
+      data.push(0);
+    else if (
+      data[data.length - 1] == multiply ||
+      data[data.length - 1] == divide
+    )
+      data.push(1);
+  }
+
+  return data;
+}
+
+function operateArray(arr) {
+  if (!arr.length) return 0;
+
+  let arrCopy = arr;
+  while (arr.length >= 3)
+    arrCopy.unshift(operate(arrCopy.shift(), arrCopy.shift(), arrCopy.shift()));
+  return arrCopy.shift();
 }
