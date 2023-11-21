707d2609f63c7329306c7975b07ca5a7b7bb267f
Charles Kwang <11234292+KojinKuro@users.noreply.github.com>
2023-11-20 22:13:46 -0800

    Style, add glow light, and fix last input bug
    Last input was grabbed at the beginning of the function but it should
    only grab it the moment it is needed so the latest version is used. A
    helper function for this purpose.  A glow light was added that triggered
    when the decimal boolean is active or the equalsMode is active. There
    has also been various CSS styling done to make the calculator feel more
    Sci-Fi feeling.

--- javascript.js
+++ javascript.js
@@ -1,12 +1,33 @@
 const calcInputNode = document.querySelector(".calculator-input");
 const calcResultNode = document.querySelector(".calculator-result");
 const calcButtonNode = document.querySelector(".calculator-buttons");
+const lightDNode = document.getElementById("light-decimal");
+const lightENode = document.getElementById("light-equal");
+
 let inputString = "";
 let decimalDisabled = false;
 let equalMode = false;
 
+window.addEventListener('light_update', () => {
+  switch(decimalDisabled) {
+    case true:
+      lightDNode.classList.add('glow');
+      break;
+    case false:
+      lightDNode.classList.remove('glow');
+      break;
+  }
+  switch(equalMode) {
+    case true:
+      lightENode.classList.add('glow');
+      break;
+    case false:
+      lightENode.classList.remove('glow');
+      break;
+  }
+});
+
 calcButtonNode.addEventListener("click", (e) => {
-  let lastInput = inputString.slice(-1);
   switch (e.target.className) {
     case "function":
       if (inputString == "") return;
@@ -16,7 +37,7 @@ calcButtonNode.addEventListener("click", (e) => {
       }
 
       decimalDisabled = false;
-      if (isOperation(lastInput)) inputString = inputString.slice(0, -1);
+      if (isOperation(getLastInput())) inputString = inputString.slice(0, -1);
       inputString += e.target.dataset.display;
       break;
     case "display":
@@ -35,30 +56,38 @@ calcButtonNode.addEventListener("click", (e) => {
         inputString = "";
       }
 
-      if (isOperation(lastInput) || lastInput == "" || lastInput == "=")
+      if (isOperation(getLastInput()) || getLastInput() == "" || getLastInput() == "=")
         inputString += "0";
       inputString += e.target.dataset.display;
       break;
     case "equal":
       if (equalMode) {
         let fArr = formatInput(inputString);
-        inputString += `${fArr[fArr.length - 2]}${fArr[fArr.length - 1]}`;
+        if (fArr.length >= 3)
+          inputString += `${fArr[fArr.length - 2]}${fArr[fArr.length - 1]}`;
       }
 
       equalMode = true;
       calcResultNode.innerText = operateArray(formatInput(inputString));
       break;
     case "clear":
-      inputString = "";
+      inputString = calcResultNode.innerText = "";
       equalMode = decimalDisabled = false;
       break;
     case "delete":
-      if (lastInput == ".") decimalDisabled = false;
+      if (getLastInput() == ".") decimalDisabled = false;
       equalMode = false;
       inputString = inputString.slice(0, -1);
   }
   calcInputNode.innerText = inputString;
+
+  const lightEvent = new Event('light_update');
+  window.dispatchEvent(lightEvent);
 });
+
+function getLastInput() {
+  return inputString.slice(-1);
+}
 
 function add(a, b) {
   console.log(`${a} + ${b} = ${a + b}`);
